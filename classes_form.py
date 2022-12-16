3e5e1418bbded430f922d6b2729134d3f67e835a
Yuriy Mironenko <ydtalel@gmail.com>
2022-12-16 16:21:38 +0500

    initial commit

---
+++ classes_form.py
@@ -0,0 +1,72 @@
+from random import *
+
+
+class Animal:
+    animals = []
+    weight_count = 0
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+        Animal.animals.append(self)
+
+    def count_class_weight(self):
+        for i in Animal.animals:
+            if isinstance(self, type(i)):
+                Animal.weight_count += i.weight
+
+        return Animal.weight_count
+
+    def feed(self):
+        return f"'{self.name}' кушает."
+
+    def make_sound(self):
+        return f"Животное '{self.name}' издаёт звук"
+
+
+class Bird(Animal):
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    def get_agg(self):
+        return f"'{self.name}' приносит вам {randint(2, 4)} яйца"
+
+
+class MilkingAnimal(Animal):
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    def get_milk(self):
+        return f"'{self.name}'  сегодня в ударе. Удой составил {randint(6, 12)} л."
+
+
+class Goose(Bird):
+    def make_sound(self):
+        return f"Гусь '{self.name}' {choice(['гогочет!', 'шипит!'])}"
+
+
+class Chicken(Bird):
+    def make_sound(self):
+        return f"Курица '{self.name}' {choice(['кудахчет!', 'ко-ко-кокает!'])}"
+
+
+class Duck(Bird):
+    def make_sound(self):
+        return f"Утка '{self.name}' крякает!"
+
+
+class Cow(MilkingAnimal):
+    def make_sound(self):
+        return f"Корова '{self.name}' говорит Mу-ууу-уу!"
+
+
+class Goat(MilkingAnimal):
+    def make_sound(self):
+        return f"Коза '{self.name}' говорит Me-eee-ee!"
+
+
+class Sheep(Animal):
+    def make_sound(self):
+        return f"Баран '{self.name}' говорит Бе-еее-ее!"
+
+    def cut(self):
+        return f"Баран '{self.name}' пострижен под {choice(['на лысо', 'пуделя'])}."
---
+++ main.py
@@ -0,0 +1,35 @@
+from classes_form import *
+
+goose_one = Goose("Серый", 5)
+goose_two = Goose("Белый", 8)
+cow = Cow("Манька", 250)
+sheep_one = Sheep("Барашек", 40)
+sheep_two = Sheep("Кудрявый", 38)
+chicken_one = Chicken("Ко-Ко", 3)
+chicken_two = Chicken("Кукареку", 2)
+goat_one = Goat("Рога", 30)
+goat_two = Goat("Копыта", 25)
+duck = Duck("Кряква", 4)
+duck_2 = Duck('fat-duck', 8000)
+
+
+animals_list = Animal.animals
+
+
+def find_heaviest(list_):
+    dict_ = {i.name: i.weight for i in list_}
+    return f"Самое тяжелое животное - {max(dict_, key=dict_.get)}"
+
+
+def get_weight_of_class():
+    d = {}
+    for animal in animals_list:
+        class_name = (str(animal).split()[0].split(".")[1].lower())
+        d[class_name] = animal.count_class_weight()
+        Animal.weight_count = 0
+    return f"общий вес всех животных(экземпляров класса) в виде словаря:\n {d}"
+
+
+print(find_heaviest(animals_list))
+print('\n')
+print(get_weight_of_class())
